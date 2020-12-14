f22104cadf60b6f60f4439fc8c8e40adbc40c40e
v3nt <dan@jynk.net>
2020-12-14 13:01:23 +0000

    hook for API

--- marvel-client/src/components/List.js
+++ marvel-client/src/components/List.js
@@ -1,14 +1,19 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 import marvel from "../apis/marvel.js";
 
 console.log(marvel);
 
 const List = () => {
-  const characters = marvel.get("", {
-    params: { limit: 20, offset: 0 },
-  });
-  //   console.log(characters);
+  const [characters, setCharacters] = useState([]);
+  const getCharacters = async () => {
+    const response = await marvel.get("/test", [], {
+      params: { limit: 20, offset: 0 },
+    });
+  };
+  getCharacters();
+  setCharacters(response.data.items);
+  console.log(response);
   return (
     <div>
       <h3>List</h3>
