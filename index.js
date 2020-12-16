9044d4afca76b00b98b146283d66f7302ddef8a4
v3nt <dan@jynk.net>
2020-12-16 11:50:18 +0000

    initial think

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const express = require("express");
 
 const bodyParser = require("body-parser");
 
+const cors = require("cors");
+const morgan = require("morgan");
+const fs = require("fs");
+const promisify = require("util").promisify;
+
 // models - not needed yet
 
 const app = express();
 app.use(bodyParser.json());
+app.use(morgan("dev"));
+app.use(cors());
+app.options("*", cors());
 
 //heroku
 if (process.env.NODE_ENV === "production") {
@@ -20,6 +28,18 @@ if (process.env.NODE_ENV === "production") {
     );
   });
 }
+
+const writeFilePromise = promisify(fs.writeFile);
+
+WriteTextToFileAsync = async (contentToWrite) => {
+  try {
+    // FIXME: give your own path, this is just a dummy path
+    const path = "\\foo.txt";
+    await writeFilePromise(contentToWrite, path);
+  } catch (err) {
+    throw new Error(`Could not write file because of {err}`);
+  }
+};
 
 app.get("/", function (req, res) {
   res.send("hello world");
